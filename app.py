b93e30aecef554f95d1b3a1dc9a0f2d5b752193d
Rusher-md <mh896299@gmail.com>
2023-07-22 19:43:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+name = input('enter your name   ')
+print(name)
+print(f"my name is {name.upper()}" if name.upper() == 'HAMZA' else 'error' )
+# if name.upper() == 'HAMZA':
+#     print(f"my name is {name.upper()}")
+# else:
+#     print('error')
