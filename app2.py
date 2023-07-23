8645f5ff98038a0fd84caffbcd38aed3be2c4c9a
Adnan Alam Ansari <adnanalam326@gmail.com>
2023-07-23 12:02:28 +0530

    Added python practice questions

--- app2.py
+++ app2.py
@@ -17,3 +17,18 @@ print({True: a, False: b} [a < b])
 # only one expression will be evaluated unlike in
 # tuple and Dictionary
 print((lambda: b, lambda: a)[a < b]())
+
+
+# Python practice
+
+Python program to add two numbers
+Maximum of two numbers in Python
+Python Program for factorial of a number
+Python Program for simple interest
+Python Program for compound interest
+Python Program to check Armstrong Number
+Python Program for Program to find area of a circle
+Python program to print all Prime numbers in an Interval
+Python program to check whether a number is Prime or not
+Python Program for n-th Fibonacci number
+
