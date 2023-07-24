46b6c362e3be4854f72fecaf9759b192ebf91b74
Rusher-md <mh896299@gmail.com>
2023-07-25 01:15:56 +0530

    addition

---
+++ new.py
@@ -0,0 +1,16 @@
+ages =  [19, 'red', 26, 23, 'age', 'yes', 12, 13.13 ,14]
+new = 'red'
+ages.insert(4, new)
+print(ages)
+new2 = 'red'
+ages.append(new2)
+print(ages)
+ages.remove(new)
+print(ages)
+ages.pop()
+print(ages)
+
+
+
+
+
