b7c8f8fec2e9b122350fa9d58a41356abe417b6b
Rusher-md <mh896299@gmail.com>
2023-08-03 19:31:15 +0530

    addition

---
+++ dictnew.py
@@ -0,0 +1,55 @@
+# my_dict = {'key1': 'R7', 'key2': 'is', 'key3': 'better', 'key4': 'in', 'key5': 'madrid', 'key6': 'not', 'key7': 'in',
+#            'key8': 'manchester', 'key9': 'the', 'key10': 'goat'}
+# print(my_dict)
+# for v in my_dict:
+#     print(v, my_dict[v])
+#     print(f"{v}:{my_dict[v]}")
+#
+# red = 'gear'
+# print(red)
+#
+# tokyo = 'rio'
+# print(tokyo)
+# print(f"{red}\\\'{tokyo}\'")
+#
+# new = (red+tokyo)
+# # print(new)
+#
+#
+# nvidia = '27584738278437847387432'
+# print(len(nvidia))
+# s = (nvidia[0:9]+" new\n\t "+nvidia[9:])
+#
+# for n in nvidia:
+#     print(type(n), n)
+#     new_num = int(n)
+#     print(new_num, type(new_num))
+
+a = '8989'
+b = '9898'
+new_u = int(a)
+new_j = int(b)
+print(new_u + new_j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ strng.py
@@ -0,0 +1,32 @@
+# values should be taken as input from the terminal
+# do maths addition of the values and print the result
+# convert the result into a list
+# convert the inputs into a tuple
+# delete the last and middle value of the list
+# check if the entered values are numeric type else display error
+
+values = input('enter the values')
+print('a', 'b', 'c')
+
+a = '123'
+b = '321'
+c = '543'
+
+new_a = int(a)
+new_b = int(b)
+new_c = int(c)
+print(type(new_a + new_b + new_c))
+#
+# new_lst = list(new_a)   #list and tuple cannot be converted beacuse int is not iterable
+# print(new_lst)
+#
+# new_tup = tuple(new_b)
+# print(new_tup)
+
+
+
+
+
+
+
+
