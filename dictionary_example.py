bc4c9bed44a7d6162339fb714b33f936bfc36946
Adnan Alam Ansari <adnanalam326@gmail.com>
2023-08-04 16:55:06 +0530

    Update dictionary_example.py

--- dictionary_example.py
+++ dictionary_example.py
@@ -15,6 +15,25 @@ valueOfGivenKey  = d['key3']
 print(valueOfGivenKey)
 
 # print key value pair of a dictionary
-
 for key in d:
     print(key, d[key])
+
+# get all items in a dictionary
+all_items = d.items()
+print(all_items)
+print(type(all_items))
+
+# conversion of dict_items to list and tuple
+d_list = list(all_items)
+print(d_list)
+
+d_tuple = tuple(all_items)
+print(d_tuple, type(d_tuple))
+
+# add key value pair
+d['key11'] = 'new value'
+print(d)
+
+# update/change value of a key
+d['key10'] = 'updated value'
+print(d)
