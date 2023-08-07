3e86ae3517f0d08b8de374284a4c90301bd0d317
Adnan Alam Ansari <adnanalam326@gmail.com>
2023-08-07 15:10:34 +0530

    initial commit

---
+++ funciton_basics.py
@@ -0,0 +1,17 @@
+# fuctions or methods
+# waf to print name from user input
+def print_name():
+    user_input = input('Type your name: ')
+    print(user_input)
+
+# waf to add two numbers
+def add_two_numbers(num1, num2):
+    addition = num1 + num2
+    print(f"Sum of two numbers is {addition}")
+    print_name()
+
+# call one fn from another
+
+
+
+add_two_numbers(12, 13)
